f9a7f2b17c1ab02b5f440ac3da57425d9f76e975
Daniel Dotsenko <ddotsenko@walnut-computing.com>
2012-04-23 13:45:08 -0700

    Changed SvgViewer sample to fit the image within the display area

--- Samples/SVGViewer/SvgViewer.Designer.cs
+++ Samples/SVGViewer/SvgViewer.Designer.cs
@@ -31,10 +31,10 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SVGViewer));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.open = new System.Windows.Forms.ToolStripButton();
-            this.svgImage = new System.Windows.Forms.PictureBox();
+            this.imageBox = new System.Windows.Forms.PictureBox();
             this.openSvgFile = new System.Windows.Forms.OpenFileDialog();
             this.toolStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.svgImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
             this.SuspendLayout();
             // 
             // toolStrip1
@@ -43,7 +43,7 @@
             this.open});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(527, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(703, 27);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -52,19 +52,20 @@
             this.open.Image = ((System.Drawing.Image)(resources.GetObject("open.Image")));
             this.open.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.open.Name = "open";
-            this.open.Size = new System.Drawing.Size(53, 22);
+            this.open.Size = new System.Drawing.Size(65, 24);
             this.open.Text = "Open";
             this.open.Click += new System.EventHandler(this.open_Click);
             // 
-            // svgImage
+            // imageBox
             // 
-            this.svgImage.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.svgImage.Location = new System.Drawing.Point(0, 25);
-            this.svgImage.Name = "svgImage";
-            this.svgImage.Size = new System.Drawing.Size(527, 449);
-            this.svgImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.svgImage.TabIndex = 1;
-            this.svgImage.TabStop = false;
+            this.imageBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox.Location = new System.Drawing.Point(0, 27);
+            this.imageBox.Margin = new System.Windows.Forms.Padding(4);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(703, 556);
+            this.imageBox.TabIndex = 1;
+            this.imageBox.TabStop = false;
+            this.imageBox.SizeChanged += new System.EventHandler(this.imageBox_SizeChanged);
             // 
             // openSvgFile
             // 
@@ -72,16 +73,17 @@
             // 
             // SVGViewer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(527, 474);
-            this.Controls.Add(this.svgImage);
+            this.ClientSize = new System.Drawing.Size(703, 583);
+            this.Controls.Add(this.imageBox);
             this.Controls.Add(this.toolStrip1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "SVGViewer";
             this.Text = "SVG Viewer";
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.svgImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,7 +93,7 @@
 
         private System.Windows.Forms.ToolStrip toolStrip1;
         private System.Windows.Forms.ToolStripButton open;
-        private System.Windows.Forms.PictureBox svgImage;
+        private System.Windows.Forms.PictureBox imageBox;
         private System.Windows.Forms.OpenFileDialog openSvgFile;
     }
 }
--- Samples/SVGViewer/SvgViewer.cs
+++ Samples/SVGViewer/SvgViewer.cs
@@ -13,21 +13,74 @@ namespace SVGViewer
 {
     public partial class SVGViewer : Form
     {
+        private string currentFile;
+
         public SVGViewer()
         {
             InitializeComponent();
+            currentFile = null;
         }
+
+        private void openFile(string fileName)
+        {
+            var displaySize = imageBox.Size;
+            
+            SvgDocument svgDoc = SvgDocument.Open(openSvgFile.FileName);
+            var svgSize = svgDoc.GetDimensions();
+
+            if (svgSize.Width == 0)
+            {
+                throw new Exception("SVG does not have size specified. Cannot work with it.");
+            }
+
+            var displayProportion = (displaySize.Height * 1.0f) / displaySize.Width;
+            var svgProportion = svgSize.Height / svgSize.Width;
+
+            float scalingFactor = 0f;
+            int padding = 10; // screen pixels
+
+            // if display is proportionally narrower than svg
+            if (displayProportion > svgProportion)
+            {
+                // we pick the width of display as max and compute the scaling against that.
+                scalingFactor = (( displaySize.Width - padding * 2 )  * 1.0f) / svgSize.Width;
+            }
+            else
+            {
+                // we pick the height of display as max and compute the scaling against that.
+                scalingFactor = ((displaySize.Height - padding * 2) * 1.0f) / svgSize.Height;
+            }
+
+            if (scalingFactor < 0)
+            {
+                throw new Exception("Viewing area is too small to render the image");
+            }
+            
+            svgDoc.Transforms = new SvgTransformCollection();
+            svgDoc.Transforms.Add(new SvgScale(scalingFactor));
+            svgDoc.Transforms.Add(new SvgTranslate(padding, padding));
+
+            //svgDoc.Width = new SvgUnit(svgDoc.Width.Type, svgDoc.Width * factor);
+            //svgDoc.Height = new SvgUnit(svgDoc.Height.Type, svgDoc.Height * factor);
+
+            imageBox.Image = svgDoc.Draw();
+        }
+
 
         private void open_Click(object sender, EventArgs e)
         {
             if (openSvgFile.ShowDialog() == DialogResult.OK)
             {
-                SvgDocument svgDoc = SvgDocument.Open(openSvgFile.FileName);
-                svgDoc.Transforms = new SvgTransformCollection();
-                svgDoc.Transforms.Add(new SvgScale(2, 2));
-                svgDoc.Width = new SvgUnit(svgDoc.Width.Type, svgDoc.Width * 2);
-                svgDoc.Height = new SvgUnit(svgDoc.Height.Type, svgDoc.Height * 2);
-                svgImage.Image = svgDoc.Draw();
+                currentFile = openSvgFile.FileName;
+                openFile(currentFile);
+            }
+        }
+
+        private void imageBox_SizeChanged(object sender, EventArgs e)
+        {
+            if (currentFile != null)
+            {
+                openFile(currentFile);
             }
         }
     }
